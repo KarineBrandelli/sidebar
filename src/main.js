8423e73ea609a92c7bd8aa562c6d8a590d64b2f9
Karine Brandelli <karinebrandelli@live.com>
2022-10-26 17:25:16 -0300

    animation modification

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ const menu = document.querySelector('.menu-bars');
 const vetLogo = document.querySelector('#logo-vet');
 const vetImage = document.querySelector('footer div');
 const hidden = document.querySelectorAll('ul li span');
+const input = document.querySelector('input');
+const navbar = document.querySelector('nav');
 
-console.log(vetImage);
+document.addEventListener('click', (event) => {
+  event.preventDefault();
+});
 
 menu.addEventListener('click', (event) => {
-  event.preventDefault();
   showMenu();
 });
 
@@ -17,4 +20,7 @@ function showMenu() {
   spanItems.map(item => item.classList.toggle('hidden'));
   vetImage.classList.toggle('hidden');
   vetLogo.classList.toggle('hidden');
-}
+  input.classList.toggle('hidden');
+  navbar.classList.toggle('sizing');
+};
+
